244cf2c2bc2df006fe47172b0546ec20e9213586
Brian N <0xb3rlin@gmail.com>
2023-02-22 18:38:09 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
